2f5c640d6a5128df1f500066bdf2febad55435d6
popori0 <saraburera6@gmail.com>
2020-05-09 22:03:32 +0900

    JavaScriptでの表を作成（仮段階）

--- progress-schedule.js
+++ progress-schedule.js
@@ -2,6 +2,7 @@
 // htmlファイルのinput要素のid属性値を指定して変数に代入
 const outputButton = document.getElementById('output');
 const resultDivided = document.getElementById('result-area');
+const tableDivided = document.getElementById('table-area')
 const startDateInput = document.getElementById('start-date')
 const endDateInput = document.getElementById('end-date')
 const startPageInput = document.getElementById('start-page')
@@ -25,14 +26,23 @@ outputButton.onclick = () => {
     
     // 出力結果表示エリアの作成
     removeAllChildren(resultDivided);
-    const header = document.createElement('h3');
-    header.innerText = '出力結果';
-    resultDivided.appendChild(header);
+    const headerResult = document.createElement('h3');
+    headerResult.innerText = '出力結果';
+    resultDivided.appendChild(headerResult);
 
     const result = studyPageDate(startDateInput.value, endDateInput.value, startPageInput.value, endPageInput.value)
     const resulttext = document.createElement('p')
-    resulttext.innerText = `開始日付から終了日付まで${result.date}日間あります。\n開始ページから終了ページまで${result.page}ページあります。\n1日あたり${result.ppd}ページ進めると終わります。`
-    resultDivided.appendChild(resulttext)
+    resulttext.innerText = `開始日付から終了日付まで${result.date}日間あります。\n開始ページから終了ページまで${result.page}ページあります。\n1日あたり${result.ppd}ページ、または1週間あたり${result.ppw}ページ進めると終わります。`
+    resultDivided.appendChild(resulttext);
+
+    // テーブルエリアの作成
+    removeAllChildren(tableDivided);
+    const haderTable = document.createElement('h3');
+    haderTable.innerText = 'スケジュール表'
+    tableDivided.appendChild(haderTable);
+    const table = createTable(10, 8);               // 第１引数で行数、第２引数で列数
+    table.border = "2";                             // ボーダーの幅
+    tableDivided.appendChild(table);
 };
 
 /**
@@ -55,11 +65,40 @@ function studyPageDate(sd, ed, sp, ep) {
     let remainingDate = Math.floor(ms / (1000 * 60 * 60 * 24) + 1);     // 残り時間をミリ秒から日へ
     let remainingPage = text.end - text.start + 1;                      // 残りのページ数
     let pagePerDate = (remainingPage / remainingDate).toFixed(2);       // 1日あたりの進行ページ数(小数第２位まで)
+    let pagePerWeek = (remainingPage * 7 / remainingDate).toFixed(2);
     // 戻り値
     let result = {
         date: remainingDate,    // 残り時間（日）
         page: remainingPage,    // 残りページ数
-        ppd: pagePerDate        // 1日あたりの進行ページ数
+        ppd: pagePerDate,        // 1日あたりの進行ページ数
+        ppw: pagePerWeek
     }
     return result;
+}
+// 表の作成
+function createTable(row, col) {
+    let table = document.createElement('table');
+    // tr(行の作成)
+    for (let i = 0; i <= row; i++) {
+        let tableRow = document.createElement('tr');
+        // td(列の作成)
+        for (let j = 0; j < col; j++) {
+            if (i === 0) {
+                let tableHead = document.createElement('th');
+                tableHead.textContent = 'Title'
+                tableRow.appendChild(tableHead);
+            } else {
+            let tableCol = document.createElement('td');
+            if (j === 0) {
+                tableCol.textContent = 'Unit' + i;
+                tableRow.appendChild(tableCol);
+            } else {
+                tableCol.textContent = 'Test' + i + '.' + j;
+                tableRow.appendChild(tableCol);
+            }
+        }
+        }
+        table.appendChild(tableRow);
+    }
+    return table;
 }
